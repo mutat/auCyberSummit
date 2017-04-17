7d4cfc101180c24dd17ddccf86140b19a7b01bcd
btatum <bryant.l.tatum@gmail.com>
2017-04-17 09:33:37 -0400

    error importing bluepring

---
+++ webapp/controllers/main.py
@@ -0,0 +1,16 @@
+from flask import (render_template, Blueprint)
+
+#from webapp.models import db, User
+
+main_blueprint = Blueprint(
+    'main',
+    __name__,
+    template_folder='../templates/main',
+    url_prefix="/"
+)
+
+
+@main_blueprint.route('/')
+def home():
+    return render_template('home.html')
+
